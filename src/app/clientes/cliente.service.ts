43bc5ec1d712342ad67e2d63c6ea659999b7febc
yfigueroa <yfsanchez@gmail.com>
2021-03-05 23:49:43 -0300

    clase 332
    eliminar

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -23,7 +23,7 @@ export class ClienteService {
     );
   }
 
-  create (cliente: Cliente) : Observable<Cliente> {
+  create(cliente: Cliente) : Observable<Cliente> {
     return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers:this.httpHeaders})
   }
 
@@ -31,7 +31,11 @@ export class ClienteService {
     return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
   }
 
-  update (cliente: Cliente) : Observable<Cliente> {
+  update(cliente: Cliente) : Observable<Cliente> {
     return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers:this.httpHeaders})
+  }
+
+  delete(id: number) : Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers:this.httpHeaders})
   }
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger'
+  },
+  buttonsStyling: false
+})
 
 @Component({
   selector: 'app-clientes',
@@ -11,12 +21,39 @@ export class ClientesComponent implements OnInit {
 
   clientes : Cliente[] = [];
 
+
   constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+    swalWithBootstrapButtons.fire({
+      title: 'Eliminando cliente...',
+      text: `Se elimina el cliente ${cliente.apellido},${cliente.nombre} !!!`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, eliminarlo',
+      cancelButtonText: 'No, cancelar',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.delete(cliente.id).subscribe(
+
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              'Eliminado.',
+              `El cliente ${cliente.apellido},${cliente.nombre} fue eliminado`,
+              'success'
+            )
+          }
+        )
+      }
+    })
   }
 
 }
